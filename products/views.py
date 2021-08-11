f0a58978d5e7b52d875365b3f7180218151f20bf
Khalil Tarabulsi <kh476379@dal.ca>
2021-08-10 23:54:03 -0300

    pushing local repo to github

---
+++ products/views.py
@@ -0,0 +1,60 @@
+from rest_framework import viewsets,status
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from .models import Product,User
+from .serializers import ProductSerializers
+
+import random
+class ProductViewSet(viewsets.ViewSet):
+    
+    # /api/products
+    def list(self,request):
+        products = Product.objects.all()
+        serializer = ProductSerializers(products,many=True)
+        
+        return Response(serializer.data)
+
+
+    # /api/products
+    def create(self,request):
+        
+        serializer = ProductSerializers(data= request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    
+    # /api/products/<str:id>
+    def retrieve(self,request,pk= None) :
+        
+        products = Product.objects.get(id =pk)
+        serializer = ProductSerializers(products)
+
+        return Response(serializer.data)
+    
+    def update(self,request,pk= None) :
+        products = Product.objects.get(id =pk)
+
+        serializer = ProductSerializers(instance=products,data= request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+
+        return Response(serializer.data, status = status.HTTP_202_ACCEPTED)
+    
+
+    def destroy(self,request,pk= None) : 
+        product = Product.objects.get(id =pk)
+
+        product.delete()
+
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+class UserAPIView(APIView):
+    
+    def get(self,_):
+        
+        users = User.objects.all() 
+        user = random.choice(users)
+        return Response({
+            'id':user.id
+        })
